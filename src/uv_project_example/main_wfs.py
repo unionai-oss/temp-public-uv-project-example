a0664bcf943a37b84045f8b810675e3e8724d2b4
Kevin Su <pingsutw@apache.org>
2025-11-07 00:15:41 -0800

    update examples
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- src/uv_project_example/main_wfs.py
+++ src/uv_project_example/main_wfs.py
@@ -6,10 +6,13 @@ from uv_project_example.library_a.utils import some_test
 from core.config import ROOT_DIR
 
 image = (
-    Image.from_debian_base(python_version=(3, 12))
+    Image.from_base("python:3.12-slim-bookworm").clone(name="flyte")
+    .with_apt_packages("git")
     .with_apt_packages("ca-certificates", "build-essential")
     .with_uv_project(pyproject_file=ROOT_DIR / "pyproject.toml")
-    .with_source_folder(ROOT_DIR / "src" / "data_loading")
+    .with_commands("mkdir -p ./src/data_loading")
+    .with_source_folder(ROOT_DIR / "src" / "data_loading", dst="./src/data_loading")
+    .with_pip_packages("git+https://github.com/flyteorg/flyte-sdk.git@7fb7d693c99ae44fc720ee78ad11ad533c5447de")
 )
 
 
@@ -19,11 +22,11 @@ env = flyte.TaskEnvironment(name="v2-uv_project", image=image)
 @env.task
 async def t1(data: str = "hello") -> str:
     i = some_test()
-    sql = ROOT_DIR / "data_loading" / "raw_data.sql"
+    sql = ROOT_DIR / "src" / "data_loading" / "raw_data.sql"
     print(f"SQL file is a file: {sql.exists()}")
     print(f"SQL file path: {sql.resolve()}")
 
-    csv = ROOT_DIR / "data_loading" / "some_data" / "my_data.csv"
+    csv = ROOT_DIR / "src" / "data_loading" / "some_data" / "my_data.csv"
     print(f"CSV file is a file: {csv.exists()}")
     print(f"CSV file path: {csv.resolve()}")
 
